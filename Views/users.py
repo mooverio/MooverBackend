4765abc79b83b688504849081598cff3aabb8121
Nils Kristian Brekke <nils.kristian.brekke@gmail.com>
2017-02-27 23:33:19 +0100

    Added example response

--- Views/users.py
+++ Views/users.py
@@ -1,6 +1,6 @@
-from __init__ import app
 from flask import jsonify
 
+from __init__ import app
 
 """ This module is intended to deliver user information through the API
 
@@ -8,7 +8,18 @@ Relevant calls include '/user', '/user/<int>'
 Prepend '/api/v0.1/' to the calls to get the correct path.
 
 Example response:
-
+{
+  "type": "user",
+  "name": "test_user1",
+  "created_at": "2011-03-19T15:42:22Z",
+  "updated_at": "2012-06-14T00:14:27Z",
+  "_links": {
+    "self": "https://api.moover.io/api/v0.1/user/test_user1"
+  },
+  "image": "http://static-cdn.moover.io/user_images/test_user1-profile_image-6947308654ad603f-300x300.jpeg",
+  "_id": 21229404,
+  "display_name": "test_user1"
+}
 
 
 """
