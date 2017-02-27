39d5809d6ac651e93beca939291c027a1ecde61b
Nils Kristian Brekke <nils.kristian.brekke@gmail.com>
2017-02-27 23:38:52 +0100

    Added example response to root API call

--- Views/root.py
+++ Views/root.py
@@ -1,5 +1,25 @@
 from __init__ import app
 
+""" Intended to be a blank response that lists all available API endpoints
+
+Response would be similar to:
+{
+  "token": {
+    "authorization": {
+      "scopes": ["user_read"],
+      "created_at": "2012-05-08T21:55:12Z",
+      "updated_at": "2012-05-17T21:32:13Z"
+    },
+    "user_name": "test_user1",
+    "valid": true
+  },
+  "_links": {
+    "user": "https://api.moover.io/api/v0.1/user"
+  }
+}
+
+"""
+
 @app.route("/")
 @app.route("/index")
 def index():
